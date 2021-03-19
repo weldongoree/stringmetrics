4ce51eb0080475a6fa11bd6331a723686962bbd0
Weldon Goree <weldon@weldon.dev>
2021-03-19 20:40:48 +0100

    setutils/makeset

--- internal/setutils/makeset.go
+++ internal/setutils/makeset.go
@@ -1,5 +1,25 @@
 package setutils
 
-func makeSet(input string) string {
-	return input
+import (
+	"strings"
+	"unicode/utf8"
+)
+
+func makeRuneSet(input string) map[rune]bool {
+	output := make(map[rune]bool)
+	for len(input) > 0 {
+		r, size := utf8.DecodeLastRuneInString(input)
+		output[r] = true
+		input = input[:len(input)-size]
+	}
+	return output
+}
+
+func makeWordSet(input string) map[string]bool {
+	output := make(map[string]bool)
+	words := strings.Fields(input)
+	for _, w := range words {
+		output[w] = true
+	}
+	return output
 }
