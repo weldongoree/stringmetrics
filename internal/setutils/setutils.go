ec5cc9ba1d3df93c908ef21e4492cce230c1725a
Weldon Goree <weldon@weldon.dev>
2021-04-22 11:20:25 +0200

    set stuff

--- internal/setutils/setutils.go
+++ internal/setutils/setutils.go
@@ -2,3 +2,23 @@ package setutils
 
 type Pmfset map[string]int
 
+func (p Pmfset) AddRunes(s string) int {
+	return len(s)
+}
+
+func (p Pmfset) AddWords(s string) int {
+	return len(s)
+}
+
+func (p Pmfset) AddBytes(s string) int {
+	return len(s)
+}
+
+func (p Pmfset) Cardinality() int {
+	return len(p)
+}
+
+func Union(p1 Pmfset, p2 Pmfset) Pmfset {
+	return p1
+}
+
